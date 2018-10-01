e6371aa0bd26bc20f0d3679bcddf27675bfd8870
cazinge <eddie.azinge@gmail.com>
2018-09-30 18:39:44 -0700

    Standardizing API access

--- mobile/src/app/App.tsx
+++ mobile/src/app/App.tsx
@@ -3,30 +3,10 @@
  */
 
 import React, { Component } from "react";
-import Amplify, { Auth } from "aws-amplify";
 import Home from "../components/Home";
-import awsconfig from "../config/aws.js";
-import gql from "graphql-tag";
-import AWSAppSyncClient from "aws-appsync";
 
-Amplify.configure(awsconfig.dev.amplify);
-
-const client = new AWSAppSyncClient(awsconfig.dev.appSync);
-
-setTimeout(() => {
-  client
-    .mutate({
-      mutation: gql`
-        mutation {
-          listPlayrolls {
-            name
-          }
-        }
-      `,
-    })
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
-}, 5000);
+// TODO: REMOVE
+import _ from "./gqlTest";
 
 export default class App extends Component {
   render() {
--- mobile/src/config/aws.js
+++ mobile/src/config/aws.js
@@ -7,20 +7,8 @@ export default {
       url: "https://wxvm74psg3.execute-api.us-west-2.amazonaws.com/dev/graphql",
       region: "us-west-2",
       auth: {
-        // type: AUTH_TYPE.API_KEY,
-        // apiKey: AppSync.apiKey
         type: AUTH_TYPE.AWS_IAM,
-        //Note - Testing purposes only
-        credentials: () =>
-          new AWS.Credentials({
-            accessKeyId: "AKIAIWAG6RLF7QMDNJGA",
-            secretAccessKey: "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",
-          }),
-        //IAM Cognito Identity using AWS Amplify
-        //credentials: () => Auth.currentCredentials(),
-        //Cognito User Pools using AWS Amplify
-        // type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
-        // jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+        credentials: () => Auth.currentCredentials(),
       },
     },
     amplify: {
@@ -30,9 +18,9 @@ export default {
         // REQUIRED - Amazon Cognito Region
         region: "us-west-2",
         // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: "XX-XXXX-X_abcd1234",
-        // OPTIONAL - Amazon Cognito Web Client ID
-        userPoolWebClientId: "XX-XXXX-X_abcd1234",
+        userPoolId: "us-west-2_u1L3OQa8W",
+        // OPTIONAL - Amazon Cognito App Client ID
+        userPoolWebClientId: "3gvfjnh67m86026e6g5vkaiegs",
       },
     },
   },

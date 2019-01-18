f90b4ad3a7c8cecdad703afc4b2bce5553d02478
Tyler Ilunga <tylerilunga1@gmail.com>
2019-01-17 23:28:50 -0800

    for review

--- services/generate/main.go
+++ services/generate/main.go
@@ -1,12 +1,74 @@
 package generate
 
 import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+
 	"github.com/cazinge/playroll/services/models"
 	"github.com/cazinge/playroll/services/models/jsonmodels"
 	"github.com/zmb3/spotify"
 
 	spotifyhelpers "github.com/cazinge/playroll/services/music_services/spotify"
 )
+
+func handleFilters(tracks []jsonmodels.MusicSource, filters *[]jsonmodels.RollFilter) error {
+	for _, filter := range *filters {
+		switch filter.Type {
+		case "Random":
+			fmt.Printf("Random [Filter Type]!")
+			for i := range tracks {
+				tracks[i] = tracks[rand.Intn(len(tracks))]
+			}
+			break
+		case "In Order":
+			fmt.Printf("In Order [Filter Type]: Don't modify!")
+			break
+		}
+	}
+	return nil
+}
+
+func handleLengths(tracks []jsonmodels.MusicSource, length *jsonmodels.RollLength) error {
+	switch length.Type {
+	case "Number":
+		fmt.Printf("Number [Length Type]!")
+
+		offset, err := strconv.ParseInt(length.Modifications[0], 10, 64)
+		if err != nil {
+			fmt.Println("offset error in handleLengths()")
+			fmt.Println(err)
+			return err
+		}
+
+		reqNumber, err := strconv.ParseInt(length.Modifications[1], 10, 64)
+		if err != nil {
+			fmt.Println("reqNumber error in handleLengths()")
+			fmt.Println(err)
+			return err
+		}
+
+		// TODO: handle throwing error on the FE when requested
+		// number of tracks is greater than the actual tracks slice length.
+		if reqNumber > int64(len(tracks)-1) {
+			return errors.New("requested # of tracks > tracks slice length")
+		}
+
+		tracks = tracks[offset:]
+		complement := int64(len(tracks)) - reqNumber
+		newTrackLength := int64(len(tracks)) - complement
+		result := make([]jsonmodels.MusicSource, newTrackLength)
+		for i := 0; i < int(newTrackLength); i++ {
+			result[i] = tracks[i]
+		}
+		tracks = result
+	case "Original":
+		fmt.Printf("Original [Length Type]: Don't modify!")
+		break
+	}
+	return nil
+}
 
 func CompileRolls(rolls *[]models.RollOutput, client *spotify.Client) (*[]models.CompiledRollOutput, error) {
 	compiledRolls := []models.CompiledRollOutput{}
@@ -41,6 +103,8 @@ func CompileRolls(rolls *[]models.RollOutput, client *spotify.Client) (*[]models
 				tracks = append(tracks, (*result)...)
 			}
 		}
+		handleFilters(tracks, &roll.Data.Filters)
+		handleLengths(tracks, &roll.Data.Length)
 		compiledRolls = append(compiledRolls, models.CompiledRollOutput{
 			Data:   jsonmodels.CompiledRollDataOutput{Tracks: tracks},
 			RollID: roll.ID,

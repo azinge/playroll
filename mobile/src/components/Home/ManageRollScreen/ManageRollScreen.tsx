ec21904788cc8d95c602bdaee09098436731da0c
cazinge <eddie.azinge@gmail.com>
2019-02-06 18:29:05 -0800

    Adding Components for future development

--- mobile/src/components/Home/ManageRollScreen/ManageRollScreen.tsx
+++ mobile/src/components/Home/ManageRollScreen/ManageRollScreen.tsx
@@ -4,12 +4,22 @@
 
 import * as React from "react";
 import { Text, SafeAreaView } from "react-native";
+import { Button } from "react-native-elements";
+import { NavigationScreenProp } from "react-navigation";
 
-export default class ManageRollScreen extends React.Component {
+export interface Props {
+  navigation?: NavigationScreenProp<{}>;
+}
+
+interface State {}
+export default class ManageRollScreen extends React.Component<Props, State> {
   render() {
     return (
       <SafeAreaView>
         <Text>ManageRollScreen</Text>
+        <Button title="Add To Playroll" onPress={() => {}} />
+        <Button title="Add To Discovery Queue" onPress={() => {}} />
+        <Button title="Recommend to Friend" onPress={() => {}} />
       </SafeAreaView>
     );
   }

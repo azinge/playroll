da5741740fa745bf3baed0bca9a79cb4985b1fd9
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-02-21 17:58:16 -0800

    styling sign in screen, but got blank screen, so fixing that now

--- mobile/src/components/Auth/SignInScreen/SignInScreen.styles.ts
+++ mobile/src/components/Auth/SignInScreen/SignInScreen.styles.ts
@@ -1,28 +1,54 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  screenContainer: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  optionsContainer: {
-    flex: 2,
+    width: '100%',
     backgroundColor: 'white',
   },
-  usernameField: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
+  container: {
+    flex: 1,
+    width: '95%',
+    paddingLeft: 20,
   },
-  passwordContainer: {
-    flexDirection: 'row',
+  inputContainer: {
+    marginTop: 25,
+    width: '100%',
+    height: 45,
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#6A0070',
+    color: '#6A0070',
   },
-  passwordField: {
-    flex: 4,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
+  showPasswordText: {
+    marginTop: 25,
+    fontSize: 14,
+    color: '#6A0070',
+  },
+  loginHeader: {
+    paddingTop: 50,
+    paddingBottom: 20,
+    paddingRight: 20,
+  },
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#6A0070',
+  },
+  submitButton: {
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    width: '100%',
+    backgroundColor: '#6A0070',
+    padding: 20,
+  },
+  submitButtonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });

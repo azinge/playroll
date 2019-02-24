8b995bb5ada01cd7662a1bb6124aa59e81710484
ianlizzo <ianliz22@icloud.com>
2019-02-24 15:55:19 -0800

    Refactored Search component

--- mobile/src/components/shared/Search/Search.tsx
+++ mobile/src/components/shared/Search/Search.tsx
@@ -20,6 +20,7 @@ import RNPickerSelect from 'react-native-picker-select';
 import { MusicSource } from '../../../graphql/types';
 import { SearchSpotifyQuery } from '../../../graphql/requests/Spotify/';
 import CreateModal from '../../Home/SearchScreen/CreateModal/CreateModal';
+import { render } from 'react-dom';
 
 const pickerStyle = StyleSheet.create({
   inputIOS: {
@@ -88,141 +89,129 @@ export default class Search extends Component<Props, State> {
   render() {
     return (
       <View>
-        <View style={styles.main}>
-          <Icon
-            name={'search'}
-            size={35}
-            color='lightgrey'
-            underlayColor='lightgrey'
-            containerStyle={{ paddingHorizontal: 5 }}
-          />
-          <TextInput
-            style={{ flex: 1, fontSize: 16 }}
-            selectionColor={'purple'}
-            placeholder='What are you looking for?'
-            onChangeText={(text: string) => this.setState({ text })}
-            onSubmitEditing={() => {
-              this.setState({ query: this.state.text });
-            }}
-            value={this.state.text}
-          />
-          {/* <View
-            style={{
-              flexDirection: 'row',
-              paddingTop: 7,
-              height: '100%',
-              margin: 0,
-              paddingLeft: 20,
-              borderLeftWidth: 1,
-              borderLeftColor: 'lightgray',
-              width: 100,
-              // height: 100%,
-            }}
-          >
-            <RNPickerSelect
-              placeholder={{}}
-              hideIcon={true}
-              items={[
-                { label: 'Artist', value: 'Artist' },
-                { label: 'Album', value: 'Album' },
-                { label: 'Track', value: 'Track' },
-                { label: 'Playlist', value: 'Playlist' },
-              ]}
-              onValueChange={value => {
-                this.setState({
-                  searchType: value,
-                });
-              }}
-              style={pickerStyle}
-            >
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ fontSize: 16 }}>{this.state.searchType}</Text>
-                <Icon name='arrow-drop-down' />
-              </View>
-            </RNPickerSelect>
-          </View> */}
-        </View>
-        <View style={{ margin: 10, flexDirection: 'row' }}>
-          <View style={{ paddingHorizontal: 7 }}>
-            <Text
-              style={[styles.options, { fontWeight: 'bold', color: '#993399' }]}
-            >
-              Popular
-            </Text>
-          </View>
-          <View style={styles.nullOptions}>
-            <Text style={[styles.options, { color: 'grey' }]}>New</Text>
-          </View>
-          <View style={styles.nullOptions}>
-            <Text style={[styles.options, { color: 'grey' }]}>ABC</Text>
-          </View>
-        </View>
+        {this.renderHeader()}
+        {this.renderOptionsBar()}
+        {this.renderModal()}
+        {this.renderSearch()}
+      </View>
+    );
+  }
 
-        <CreateModal
-          currentSource={this.state.currentSource}
-          modalVisible={this.state.modalVisible}
-          closeModal={this.closeModal}
-          playrollID={this.props.playrollID}
-          navigation={this.props.navigation}
+  renderHeader() {
+    return (
+      <View style={styles.main}>
+        <Icon
+          name={'search'}
+          size={35}
+          color='lightgrey'
+          underlayColor='lightgrey'
+          containerStyle={{ paddingHorizontal: 5 }}
         />
-        <SearchSpotifyQuery
-          variables={{
-            query: this.state.query,
-            searchType: this.state.searchType,
+        <TextInput
+          style={{ flex: 1, fontSize: 16 }}
+          selectionColor={'purple'}
+          placeholder='What are you looking for?'
+          onChangeText={(text: string) => this.setState({ text })}
+          onSubmitEditing={() => {
+            this.setState({ query: this.state.text });
           }}
-        >
-          {({ loading, error, data }) => {
-            return (
-              <View style={{ marginBottom: 145 }}>
-                <FlatList
-                  data={data && data.searchSpotify}
-                  showsVerticalScrollIndicator={false}
-                  keyExtractor={(item, index) => item.providerID}
-                  extraData={this.state}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      onPress={() => this.setModal(item)}
+          value={this.state.text}
+        />
+      </View>
+    );
+  }
+
+  renderOptionsBar() {
+    return (
+      <View style={{ margin: 10, flexDirection: 'row' }}>
+        <View style={{ paddingHorizontal: 7 }}>
+          <Text
+            style={[styles.options, { fontWeight: 'bold', color: '#993399' }]}
+          >
+            Popular
+          </Text>
+        </View>
+        <View style={styles.nullOptions}>
+          <Text style={[styles.options, { color: 'grey' }]}>New</Text>
+        </View>
+        <View style={styles.nullOptions}>
+          <Text style={[styles.options, { color: 'grey' }]}>ABC</Text>
+        </View>
+      </View>
+    );
+  }
+
+  renderModal() {
+    return (
+      <CreateModal
+        currentSource={this.state.currentSource}
+        modalVisible={this.state.modalVisible}
+        closeModal={this.closeModal}
+        playrollID={this.props.playrollID}
+        navigation={this.props.navigation}
+      />
+    );
+  }
+
+  renderSearch() {
+    return (
+      <SearchSpotifyQuery
+        variables={{
+          query: this.state.query,
+          searchType: this.state.searchType,
+        }}
+      >
+        {({ loading, error, data }) => {
+          return (
+            <View style={{ marginBottom: 145 }}>
+              <FlatList
+                data={data && data.searchSpotify}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item, index) => item.providerID}
+                extraData={this.state}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => this.setModal(item)}
+                    key={item.providerID}
+                  >
+                    <View
+                      style={{ width: '100%', alignItems: 'center' }}
                       key={item.providerID}
                     >
-                      <View
-                        style={{ width: '100%', alignItems: 'center' }}
-                        key={item.providerID}
-                      >
-                        <View style={{ flexDirection: 'row', width: '100%' }}>
-                          <Image
-                            style={styles.cover}
-                            source={{ uri: item.cover }}
-                          />
-                          <View style={{ flex: 1, justifyContent: 'center' }}>
-                            <Text style={styles.artist} numberOfLines={2}>
-                              {item.name}
+                      <View style={{ flexDirection: 'row', width: '100%' }}>
+                        <Image
+                          style={styles.cover}
+                          source={{ uri: item.cover }}
+                        />
+                        <View style={{ flex: 1, justifyContent: 'center' }}>
+                          <Text style={styles.artist} numberOfLines={2}>
+                            {item.name}
+                          </Text>
+                          {item.creator ? (
+                            <Text style={styles.noArtist} numberOfLines={2}>
+                              {item.creator}
                             </Text>
-                            {item.creator ? (
-                              <Text style={styles.noArtist} numberOfLines={2}>
-                                {item.creator}
-                              </Text>
-                            ) : null}
-                          </View>
-                          <Icon
-                            size={35}
-                            name='more-vert'
-                            color='lightgrey'
-                            onPress={() =>
-                              this.props.navigation &&
-                              this.props.navigation.goBack(null)
-                            }
-                          />
+                          ) : null}
                         </View>
-                        <View style={styles.spacing} />
+                        <Icon
+                          size={35}
+                          name='more-vert'
+                          color='lightgrey'
+                          onPress={() =>
+                            this.props.navigation &&
+                            this.props.navigation.goBack(null)
+                          }
+                        />
                       </View>
-                    </TouchableOpacity>
-                  )}
-                />
-              </View>
-            );
-          }}
-        </SearchSpotifyQuery>
-      </View>
+                      <View style={styles.spacing} />
+                    </View>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
+          );
+        }}
+      </SearchSpotifyQuery>
     );
   }
 }

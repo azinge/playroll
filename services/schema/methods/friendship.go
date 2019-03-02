13dfe941475e7cbfbd6b90c8fb50f25e4c116d3f
JigarSwam <jswaminarayan@gmail.com>
2019-03-01 22:03:41 -0800

    Implemented AuthorizeUser to friendship.go

--- services/schema/methods/friendship.go
+++ services/schema/methods/friendship.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/cazinge/playroll/services/gqltag"
 	"github.com/cazinge/playroll/services/models"
-	"github.com/cazinge/playroll/services/utils"
 	"github.com/graphql-go/graphql"
-	"github.com/mitchellh/mapstructure"
 )
 
 // FriendshipMethods defines the types for all friendship methods.
@@ -18,18 +16,12 @@ type FriendshipMethods struct {
 var browseUserFriends = gqltag.Method{
 	Description: `Browses the current users friends.`,
 	Request: func(resolveParams graphql.ResolveParams, mctx *gqltag.MethodContext) (interface{}, error) {
-		// Destructers your parameters that the function takes in and makes avail through param object.
-		type browseUserFriendsParams struct {
-			UserID string
-		}
-		params := &browseUserFriendsParams{}
-		err := mapstructure.Decode(resolveParams.Args, params)
+		user, err := models.AuthorizeUser(mctx)
 		if err != nil {
 			fmt.Println(err)
 			return nil, err
 		}
-
-		userID := utils.StringIDToNumber(params.UserID)
+		userID := user.ID
 
 		users, err := models.GetFriendsByUserID(userID, mctx.DB)
 		if err != nil {

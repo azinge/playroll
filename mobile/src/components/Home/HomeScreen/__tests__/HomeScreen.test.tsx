ff25d00e0f9bf73b2a8b25f084cbc9990810113f
ianlizzo <ianliz22@icloud.com>
2019-03-07 18:36:47 -0800

    re-commit

--- mobile/src/components/Home/HomeScreen/__tests__/HomeScreen.test.tsx
+++ mobile/src/components/Home/HomeScreen/__tests__/HomeScreen.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-import DiscoverScreen from '../DiscoverScreen';
+import HomeScreen from '../HomeScreen';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<DiscoverScreen />).toJSON();
+  const tree = renderer.create(<HomeScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });

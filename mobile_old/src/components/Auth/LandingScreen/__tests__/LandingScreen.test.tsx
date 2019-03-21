405e72627184bab4d28fce8a2d07b84d384e1ee2
Masao Kitamura <masaok@gmail.com>
2019-03-20 23:12:58 -0700

    removing mobile_old

--- mobile_old/src/components/Auth/LandingScreen/__tests__/LandingScreen.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import renderer from "react-test-renderer";
-
-import LandingScreen from "../LandingScreen";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<LandingScreen />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- mobile_old/src/components/Home/ManageRollScreen/__tests__/ManageRollScreen.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import renderer from "react-test-renderer";
-
-import ManageRollScreen from "../ManageRollScreen";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<ManageRollScreen />).toJSON();
-  expect(tree).toMatchSnapshot();
-});

d31834b4d5fa707759d7f0ad5b88925ef29ba21d
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-04-10 19:39:34 -0700

    Added refresh control to view playroll screen

--- mobile/src/components/Library/ViewPlayrollScreen/ViewPlayrollScreen.tsx
+++ mobile/src/components/Library/ViewPlayrollScreen/ViewPlayrollScreen.tsx
@@ -2,25 +2,25 @@
  * ViewPlayrollScreen
  */
 
-import * as React from 'react';
-import { View, TextInput, ScrollView, Image } from 'react-native';
-import { Header, Icon } from 'react-native-elements';
-import { NavigationScreenProp } from 'react-navigation';
-import styles, { rawStyles } from './ViewPlayrollScreen.styles';
-import { Playroll, MusicSource } from '../../../graphql/types';
+import * as React from "react";
+import { View, TextInput, ScrollView, Image } from "react-native";
+import { Header, Icon } from "react-native-elements";
+import { NavigationScreenProp } from "react-navigation";
+import styles, { rawStyles } from "./ViewPlayrollScreen.styles";
+import { Playroll, MusicSource } from "../../../graphql/types";
 
 import {
   UpdatePlayrollMutation,
-  GetCurrentUserPlayrollQuery,
-} from '../../../graphql/requests/Playroll';
-import { GenerateTracklistMutation } from '../../../graphql/requests/Tracklist';
+  GetCurrentUserPlayrollQuery
+} from "../../../graphql/requests/Playroll";
+import { GenerateTracklistMutation } from "../../../graphql/requests/Tracklist";
 
-import { GET_CURRENT_USER_PLAYROLL } from '../../../graphql/requests/Playroll/GetCurrentUserPlayrollQuery';
-import SubScreenContainer from '../../shared/Containers/SubScreenContainer';
-import SubScreenHeader from '../../shared/Headers/SubScreenHeader';
-import Icons from '../../../themes/Icons';
-import NavigationService from '../../../services/NavigationService';
-import RollList from '../../shared/Lists/RollList';
+import { GET_CURRENT_USER_PLAYROLL } from "../../../graphql/requests/Playroll/GetCurrentUserPlayrollQuery";
+import SubScreenContainer from "../../shared/Containers/SubScreenContainer";
+import SubScreenHeader from "../../shared/Headers/SubScreenHeader";
+import Icons from "../../../themes/Icons";
+import NavigationService from "../../../services/NavigationService";
+import RollList from "../../shared/Lists/RollList";
 
 export interface Props {
   navigation?: NavigationScreenProp<{}>;
@@ -34,7 +34,7 @@ export default class ViewPlayrollScreen extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      editPlayrollName: '',
+      editPlayrollName: ""
     };
     this.renderHeader = this.renderHeader.bind(this);
   }
@@ -43,11 +43,11 @@ export default class ViewPlayrollScreen extends React.Component<Props, State> {
     const playrollID: number =
       (this.props &&
         this.props.navigation &&
-        this.props.navigation.getParam('playroll').id) ||
+        this.props.navigation.getParam("playroll").id) ||
       {};
     return (
       <SubScreenContainer
-        title='View Playroll'
+        title="View Playroll"
         renderHeader={this.renderHeader}
       >
         <GetCurrentUserPlayrollQuery variables={{ id: playrollID }}>
@@ -69,24 +69,24 @@ export default class ViewPlayrollScreen extends React.Component<Props, State> {
     const playroll: Playroll =
       (this.props &&
         this.props.navigation &&
-        this.props.navigation.getParam('playroll')) ||
+        this.props.navigation.getParam("playroll")) ||
       {};
 
     const addRollIcon = {
       ...Icons.addIcon,
-      onPress: () => NavigationService.navigate('EditRoll'),
+      onPress: () => NavigationService.navigate("EditRoll")
     };
     const editPlayrollIcon = {
       ...Icons.settingsIcon,
       onPress: () =>
-        NavigationService.navigate('EditPlayroll', {
-          managePlayroll: 'View Playroll',
-          playroll,
-        }),
+        NavigationService.navigate("EditPlayroll", {
+          managePlayroll: "View Playroll",
+          playroll
+        })
     };
     return (
       <SubScreenHeader
-        title={'View Playroll'}
+        title={"View Playroll"}
         icons={[addRollIcon, editPlayrollIcon, Icons.menuIcon]}
       />
     );
@@ -96,24 +96,24 @@ export default class ViewPlayrollScreen extends React.Component<Props, State> {
       <View style={styles.editingBarContainer}>
         <Image
           style={rawStyles.editingBarImage}
-          source={require('../../../assets/new_playroll.png')}
+          source={require("../../../assets/new_playroll.png")}
         />
         <UpdatePlayrollMutation
           variables={{
             id: playroll.id,
             input: {
               name: this.state.editPlayrollName,
-              userID: playroll.userID,
-            },
+              userID: playroll.userID
+            }
           }}
           refetchQueries={[GET_CURRENT_USER_PLAYROLL]}
         >
           {(updatePlayroll, { data }) => (
             <View style={styles.editingBarNameContainer}>
               <TextInput
-                selectionColor={'purple'}
-                placeholder='Name Your Playroll'
-                placeholderTextColor='lightgrey'
+                selectionColor={"purple"}
+                placeholder="Name Your Playroll"
+                placeholderTextColor="lightgrey"
                 style={styles.editingBarNameInput}
                 onChangeText={name => this.setState({ editPlayrollName: name })}
                 onSubmitEditing={() => updatePlayroll()}
@@ -122,9 +122,9 @@ export default class ViewPlayrollScreen extends React.Component<Props, State> {
               </TextInput>
               <View style={styles.horizontalRule} />
               <TextInput
-                selectionColor={'purple'}
-                placeholder='#Existential #Chill #Help'
-                placeholderTextColor='lightgrey'
+                selectionColor={"purple"}
+                placeholder="#Existential #Chill #Help"
+                placeholderTextColor="lightgrey"
                 style={styles.editingBarTagInput}
               />
             </View>

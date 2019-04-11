8cf8f71a05ea2ccd69c40ae77738f97e8bf744c4
Tyler Ilunga <tylerilunga1@gmail.com>
2019-04-10 20:07:47 -0700

    deleting unused code

--- mobile/src/graphql/requests/User/SearchUsersQuery.ts
+++ mobile/src/graphql/requests/User/SearchUsersQuery.ts
@@ -2,19 +2,7 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { User, UserFragments } from '../../types';
 
-export const GET_CURRENT_USER = 'GET_CURRENT_USER';
 export const SEARCH_USERS = 'SEARCH_USERS';
-
-export const GET_CURRENT_USER_QUERY = gql`
-  query GET_CURRENT_USER {
-    private {
-      currentUser {
-        ...DefaultUser
-      }
-    }
-  }
-  ${UserFragments.default}
-`;
 
 export const SEARCH_USERS_QUERY = gql`
   query SEARCH_USERS($query: String, $offset: Int, $count: Int) {
@@ -27,20 +15,10 @@ export const SEARCH_USERS_QUERY = gql`
   ${UserFragments.default}
 `;
 
-type GetCurrentUserVariables = {
-  id?: number;
-};
-
 type SearchUsersVariables = {
   query: string;
   offset?: number;
   count?: number;
-};
-
-type GetCurrentUserData = {
-  private: {
-    currentUser?: User;
-  };
 };
 
 type SearchUsersData = {

e0ef2b04818f0c74fca31f5667497b464126a09e
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-04-11 23:10:53 -0700

    Redesigned Account Screen

--- mobile/src/components/Profile/AccountScreen/AccountScreen.tsx
+++ mobile/src/components/Profile/AccountScreen/AccountScreen.tsx
@@ -2,20 +2,27 @@
  * AccountScreen
  */
 
-import * as React from 'react';
-import { Text, View, SafeAreaView, Image } from 'react-native';
+import * as React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import { ListItem } from "react-native-elements";
 
-import styles from './AccountScreen.styles';
-import { SignOutMutation } from '../../../graphql/requests/Auth';
-import { GetCurrentUserQuery } from '../../../graphql/requests/User';
+import styles from "./AccountScreen.styles";
+import { SignOutMutation } from "../../../graphql/requests/Auth";
+import { GetCurrentUserQuery } from "../../../graphql/requests/User";
 import {
   NavigationScreenProp,
   StackActions,
-  NavigationActions,
-} from 'react-navigation';
-import NavigationService from '../../../services/NavigationService';
-import SubScreenContainer from '../../shared/Containers/SubScreenContainer';
-import { Button } from 'react-native-elements';
+  NavigationActions
+} from "react-navigation";
+import NavigationService from "../../../services/NavigationService";
+import SubScreenContainer from "../../shared/Containers/SubScreenContainer";
+import { Button } from "react-native-elements";
 
 export interface Props {
   navigation?: NavigationScreenProp<{}>;
@@ -23,80 +30,141 @@ export interface Props {
 
 interface State {}
 
+const list = [{}];
+
 export default class AccountScreen extends React.Component<Props, State> {
+  // render() {
+  //   return (
+  //     <GetCurrentUserQuery>
+  //       {({ loading, error, data }) => {
+  //         if (loading || error || Object.keys(data).length === 0) {
+  //           return <SubScreenContainer title="My Account" modal />;
+  //         }
+  //         const currentUser = (data && data.private.currentUser) || {};
+  //         return (
+  //           <SubScreenContainer title="My Account" modal>
+  //             <View style={{ alignItems: "left", marginVertical: 20 }}>
+  //               <Image
+  //                 source={{
+  //                   uri: currentUser.avatar
+  //                 }}
+  //                 style={{ height: 100, width: 100, borderRadius: 5 }}
+  //               />
+  //               <Text
+  //                 style={{
+  //                   marginLeft: 20,
+  //                   fontWeight: "bold",
+  //                   fontSize: 20,
+  //                   color: "#993399"
+  //                 }}
+  //               >
+  //                 {currentUser.name}
+  //               </Text>
+  //             </View>
+  //           </SubScreenContainer>
+  //         );
+  //       }}
+  //     </GetCurrentUserQuery>
+  //   );
+  // }
   render() {
     return (
       <GetCurrentUserQuery>
         {({ loading, error, data }) => {
           if (loading || error || Object.keys(data).length === 0) {
-            return <SubScreenContainer title='My Account' modal />;
+            return <SubScreenContainer title="My Account" modal />;
           }
           const currentUser = (data && data.private.currentUser) || {};
           return (
-            <SubScreenContainer title='My Account' modal>
-              <View style={{ alignItems: 'center' }}>
-                <View style={{ alignItems: 'center', marginVertical: 20 }}>
-                  <Image
-                    source={{
-                      uri: currentUser.avatar,
-                    }}
-                    style={{ height: 100, width: 100, borderRadius: 5 }}
-                  />
-                  <Text
-                    style={{
-                      fontWeight: 'bold',
-                      fontSize: 20,
-                      color: '#993399',
-                    }}
-                  >
-                    {currentUser.name}
-                  </Text>
-                </View>
-                <Button
-                  style={{ marginVertical: 3 }}
-                  title='Connect Spotify'
-                  onPress={() => {
-                    NavigationService.navigate('ConnectSpotify');
+            <SubScreenContainer title="My Account" modal>
+              <View
+                style={{
+                  marginLeft: 15,
+                  marginTop: 10,
+                  flex: 1,
+                  marginBottom: 5,
+                  flexDirection: "row",
+                  alignItems: "center"
+                }}
+              >
+                <Image
+                  source={{
+                    // uri: currentUser.avatar
+                    uri:
+                      "https://media.pitchfork.com/photos/5b64cdcd0b743703a4982025/2:1/w_790/Travis-Scott.jpg"
                   }}
+                  style={{ height: 60, width: 60, borderRadius: 30 }}
                 />
-                <Button
-                  style={{ marginVertical: 3 }}
-                  title='My Public Profile'
-                  onPress={() => {
-                    NavigationService.navigate('ViewProfile');
+
+                <Text
+                  style={{
+                    marginLeft: 15,
+                    marginTop: 5,
+                    fontWeight: "bold",
+                    fontSize: 20,
+                    color: "#993399"
                   }}
-                  disabled
-                />
-                <Button
-                  style={{ marginVertical: 3 }}
-                  title='My Friends'
+                >
+                  {/* {currentUser.name} */}
+                  {"Nicolas Raymundo"}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  marginTop: 10
+                }}
+              >
+                {/* View Profile */}
+                <TouchableOpacity
                   onPress={() => {
-                    NavigationService.navigate('FriendsMenu');
+                    NavigationService.navigate("ViewProfile");
                   }}
-                  disabled
-                />
-                <Button
-                  style={{ marginVertical: 3 }}
-                  title='Edit Profile'
+                >
+                  <View style={styles.textContainer}>
+                    <Text style={styles.enabledText}>View Profile</Text>
+                  </View>
+                </TouchableOpacity>
+
+                {/* View Friends */}
+                <TouchableOpacity
                   onPress={() => {
-                    NavigationService.navigate('EditProfile');
+                    NavigationService.navigate("FriendsMenu");
                   }}
-                  disabled
-                />
-                <Button
-                  style={{ marginVertical: 3 }}
-                  title='Settings'
+                >
+                  <View style={styles.textContainer}>
+                    <Text style={styles.enabledText}>View Friends</Text>
+                  </View>
+                </TouchableOpacity>
+
+                {/* Edit Profile */}
+                <TouchableOpacity
                   onPress={() => {
-                    NavigationService.navigate('Settings');
+                    NavigationService.navigate("EditProfile");
                   }}
-                  disabled
-                />
+                >
+                  <View style={styles.textContainer}>
+                    <Text style={styles.enabledText}>Edit Profile</Text>
+                  </View>
+                </TouchableOpacity>
+
+                {/* Settings */}
+                <TouchableOpacity
+                  onPress={() => {
+                    NavigationService.navigate("Settings");
+                  }}
+                >
+                  <View style={styles.textContainer}>
+                    <Text style={styles.enabledText}>Settings</Text>
+                  </View>
+                </TouchableOpacity>
+
+                {/* Sign Out */}
+
                 <SignOutMutation>
                   {signOut => {
                     return (
-                      <Button
-                        style={{ marginVertical: 50 }}
-                        title='Sign Out'
+                      <TouchableOpacity
                         onPress={() => {
                           signOut().then(
                             () =>
@@ -107,14 +175,18 @@ export default class AccountScreen extends React.Component<Props, State> {
                                   index: 0,
                                   actions: [
                                     NavigationActions.navigate({
-                                      routeName: 'Auth',
-                                    }),
-                                  ],
+                                      routeName: "Auth"
+                                    })
+                                  ]
                                 })
                               )
                           );
                         }}
-                      />
+                      >
+                        <View style={styles.textContainer}>
+                          <Text style={styles.signOut}>Sign Out</Text>
+                        </View>
+                      </TouchableOpacity>
                     );
                   }}
                 </SignOutMutation>
@@ -126,3 +198,110 @@ export default class AccountScreen extends React.Component<Props, State> {
     );
   }
 }
+// return (
+//   <GetCurrentUserQuery>
+//     {({ loading, error, data }) => {
+//       if (loading || error || Object.keys(data).length === 0) {
+//         return <SubScreenContainer title="My Account" modal />;
+//       }
+//       const currentUser = (data && data.private.currentUser) || {};
+//       return (
+//         <SubScreenContainer title="My Account" modal>
+//           <View style={{ alignItems: "left" }}>
+//             <View style={{ alignItems: "center", marginVertical: 20 }}>
+//               <Image
+//                 source={{
+//                   uri: currentUser.avatar
+//                 }}
+//                 style={{ height: 100, width: 100, borderRadius: 5 }}
+//               />
+//               <Text
+//                 style={{
+//                   fontWeight: "bold",
+//                   fontSize: 20,
+//                   color: "#993399"
+//                 }}
+//               >
+//                 {currentUser.name}
+//               </Text>
+//             </View>
+
+//             <Button
+//               style={{ marginVertical: 3 }}
+//               title="Connect Spotify"
+//               onPress={() => {
+//                 NavigationService.navigate("ConnectSpotify");
+//               }}
+//             />
+
+//             <Button
+//               style={{ marginVertical: 3 }}
+//               title="My Public Profile"
+//               onPress={() => {
+//                 NavigationService.navigate("ViewProfile");
+//               }}
+//               disabled
+//             />
+
+//             <Button
+//               style={{ marginVertical: 3 }}
+//               title="My Friends"
+//               onPress={() => {
+//                 NavigationService.navigate("FriendsMenu");
+//               }}
+//               disabled
+//             />
+
+//             <Button
+//               style={{ marginVertical: 3 }}
+//               title="Edit Profile"
+//               onPress={() => {
+//                 NavigationService.navigate("EditProfile");
+//               }}
+//               disabled
+//             />
+
+//             <Button
+//               style={{ marginVertical: 3 }}
+//               title="Settings"
+//               onPress={() => {
+//                 NavigationService.navigate("Settings");
+//               }}
+//               disabled
+//             />
+
+//             <SignOutMutation>
+//               {signOut => {
+//                 return (
+//                   <Button
+//                     style={{ marginVertical: 50 }}
+//                     title="Sign Out"
+//                     onPress={() => {
+//                       signOut().then(
+//                         () =>
+//                           this.props.navigation &&
+//                           this.props.navigation.dispatch(
+//                             StackActions.reset({
+//                               key: null,
+//                               index: 0,
+//                               actions: [
+//                                 NavigationActions.navigate({
+//                                   routeName: "Auth"
+//                                 })
+//                               ]
+//                             })
+//                           )
+//                       );
+//                     }}
+//                   />
+//                 );
+//               }}
+//             </SignOutMutation>
+//           </View>
+//         </SubScreenContainer>
+//       );
+//     }}
+//   </GetCurrentUserQuery>
+// );
+//   }
+// }

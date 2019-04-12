89ec44ba7b6740406e0dfb9c479eec8986bb541b
it-door <tedchu4@gmail.com>
2019-04-11 18:46:04 -0700

    Began ProfileScreenContainer

--- mobile/src/components/Profile/ViewProfileScreen/ViewProfileScreen.tsx
+++ mobile/src/components/Profile/ViewProfileScreen/ViewProfileScreen.tsx
@@ -2,56 +2,61 @@
  * ViewProfileScreen
  */
 
-import * as React from 'react';
-import SubScreenContainer from '../../shared/Containers/SubScreenContainer';
-import { Text, View, SafeAreaView, Image, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native';
-import { GetCurrentUserQuery } from '../../../graphql/requests/User';
-import PlayrollCard from '../../shared/Cards/PlayrollCard';
-import { Button, ListItem } from 'react-native-elements';
-import { Playroll } from '../../../graphql/types';
-import Icons from '../../../themes/Icons';
-import { LIST_CURRENT_USER_PLAYROLLS } from '../../../graphql/requests/Playroll/ListCurrentUserPlayrollsQuery';
-import PlaceholderList from '../../shared/Lists/PlaceholderList';
+import * as React from "react";
+import SubScreenContainer from "../../shared/Containers/SubScreenContainer";
+import ProfileScreenContainer from "../../shared/Containers/ProfileScreenContainer";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import { GetCurrentUserQuery } from "../../../graphql/requests/User";
+import PlayrollCard from "../../shared/Cards/PlayrollCard";
+import { Button, ListItem } from "react-native-elements";
+import { Playroll } from "../../../graphql/types";
+import Icons from "../../../themes/Icons";
+import { LIST_CURRENT_USER_PLAYROLLS } from "../../../graphql/requests/Playroll/ListCurrentUserPlayrollsQuery";
+import PlaceholderList from "../../shared/Lists/PlaceholderList";
 
 import {
   ListCurrentUserPlayrollsQuery,
-  CreatePlayrollMutation,
-} from '../../../graphql/requests/Playroll/';
-import NavigationService from '../../../services/NavigationService';
+  CreatePlayrollMutation
+} from "../../../graphql/requests/Playroll/";
+import NavigationService from "../../../services/NavigationService";
 
 export default class ViewProfileScreen extends React.Component {
   _renderItem = ({ item }) => (
     <TouchableOpacity
       style={{ marginHorizontal: 20, marginBottom: 5, marginTop: 10 }}
-      onPress={() =>
-        NavigationService.navigate('ViewPlayroll', {
-          
-        })
-      }
+      onPress={() => NavigationService.navigate("ViewPlayroll", {})}
     >
       <ListItem
         title={item.name}
-        titleStyle={{ color: 'purple' }}
+        titleStyle={{ color: "purple" }}
         subtitle={item.type}
         leftAvatar={{
-          source: { uri: item.cover },
+          source: { uri: item.cover }
         }}
         containerStyle={{
-          borderColor: 'white',
+          borderColor: "white",
           borderRadius: 10,
-          shadowColor: 'gray',
+          shadowColor: "gray",
           shadowOffset: {
             width: 2,
-            height: 1,
+            height: 1
           },
           shadowRadius: 3,
           shadowOpacity: 0.2,
-          overflow: 'visible',
+          overflow: "visible"
         }}
       />
     </TouchableOpacity>
-  )
-  
+  );
+
   render() {
     const extractPlayrolls = data => {
       if (
@@ -63,75 +68,77 @@ export default class ViewProfileScreen extends React.Component {
       return data.private.listCurrentUserPlayrolls;
     };
     return (
-      <View style={{flex: 1}}>
-      <GetCurrentUserQuery>
-        {({ loading, error, data }) => {
-          if (loading || error || Object.keys(data).length === 0) {
-            return <SubScreenContainer title='My Account' modal />;
-          }
-          const currentUser = (data && data.private.currentUser) || {};
-          return (
-            <SubScreenContainer title='My Account' modal>
-              <View style={{ alignItems: 'center' }}>
-                <View style={{ alignItems: 'center', marginVertical: 20 }}>
-                  <Image
-                    source={{
-                      uri: currentUser.avatar,
-                    }}
-                    style={{ height: 100, width: 100, borderRadius: 5 }}
-                  />
-                  <Text
-                    style={{
-                      fontWeight: 'bold',
-                      fontSize: 20,
-                      color: '#993399',
-                    }}
-                  >
-                    {currentUser.name}
-                  </Text>
-                </View>
-              </View>
-            </SubScreenContainer>
-          );
-        }}
-      </GetCurrentUserQuery>
-            <ListCurrentUserPlayrollsQuery>
-            {({ loading, error, data }) => {
-              const playrolls = extractPlayrolls(data);
-              const success = !loading && !error;
-              return (
-                <View
-                  style={{
-                    flex: 1,
-                    // TODO(ianlizzo): Fix this pls
-                    // marginBottom: 30,
-                  }}
-                >
-                  <View style={{ flex: 1 }}>
-                    <FlatList
-                      data = {playrolls}
-                      keyExtractor = {(item, index) => item.id}
-                      renderItem={( this._renderItem )}
+      <View style={{ flex: 1 }}>
+        <GetCurrentUserQuery>
+          {({ loading, error, data }) => {
+            if (loading || error || Object.keys(data).length === 0) {
+              return <SubScreenContainer title="My Public Profile" modal />;
+            }
+            const currentUser = (data && data.private.currentUser) || {};
+            return (
+              <ProfileScreenContainer title="My Public Profile" modal>
+                <View style={{ alignItems: "center" }}>
+                  <View style={{ alignItems: "center", marginVertical: 20 }}>
+                    <Image
+                      source={{
+                        uri: currentUser.avatar
+                      }}
+                      style={{ height: 100, width: 100, borderRadius: 5 }}
                     />
-                      {loading && (
-                        <ActivityIndicator
-                          color={'gray'}
-                          style={{ paddingTop: 50 }}
-                        />
-                      )}
-                      {error && (
-                        <Text style={{ paddingTop: 50 }}>
-                          Error Loading Playrolls
-                        </Text>
-                      )}
-                      {/* <View style={{ margin: 10 }} /> */}
-                    {playrolls.length === 0 && <Text> No Playrolls added</Text>}
+                    <Text
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: 20,
+                        color: "#993399"
+                      }}
+                    >
+                      {currentUser.name}
+                    </Text>
                   </View>
                 </View>
-              );
-            }}
-          </ListCurrentUserPlayrollsQuery>
-        </View>
+                <ListCurrentUserPlayrollsQuery>
+                  {({ loading, error, data }) => {
+                    const playrolls = extractPlayrolls(data);
+                    const success = !loading && !error;
+                    return (
+                      <View
+                        style={{
+                          flex: 1
+                          // TODO(ianlizzo): Fix this pls
+                          // marginBottom: 30,
+                        }}
+                      >
+                        <View style={{ flex: 1 }}>
+                          <FlatList
+                            data={playrolls}
+                            keyExtractor={(item, index) => item.id}
+                            renderItem={this._renderItem}
+                          />
+                          {loading && (
+                            <ActivityIndicator
+                              color={"gray"}
+                              style={{ paddingTop: 50 }}
+                            />
+                          )}
+                          {error && (
+                            <Text style={{ paddingTop: 50 }}>
+                              Error Loading Playrolls
+                            </Text>
+                          )}
+                          {/* <View style={{ margin: 10 }} /> */}
+                          {playrolls.length === 0 && (
+                            <Text> No Playrolls added</Text>
+                          )}
+                        </View>
+                      </View>
+                    );
+                  }}
+                </ListCurrentUserPlayrollsQuery>
+              </ProfileScreenContainer>
+            );
+          }}
+        </GetCurrentUserQuery>
+      </View>
     );
   }
 }
---
+++ mobile/src/components/shared/Containers/ProfileScreenContainer/ProfileScreenContainer.tsx
@@ -0,0 +1,80 @@
+/**
+ * ProfileScreenContainer
+ */
+
+import * as React from 'react';
+import Collapsible from 'react-native-collapsible-header';
+import { isIphoneX } from 'react-native-iphone-x-helper';
+import SubScreenHeader from '../../Headers/SubScreenHeader';
+import { HeaderIconType } from '../../../../themes/Icons';
+import { SafeAreaView } from 'react-navigation';
+import { ViewStyle, StyleProp } from 'react-native';
+
+interface HeaderProps {
+  title?: string;
+  modal?: boolean;
+  icons?: HeaderIconType[];
+}
+
+interface ContainerProps extends HeaderProps {
+  renderHeader?: () => JSX.Element;
+  flatList?: boolean;
+  contentContainerStyle?: StyleProp<ViewStyle>;
+  data?: any[];
+  keyExtractor?: (item: any, i: number) => string;
+  renderItem?: (obj: any) => any;
+}
+
+interface Props extends ContainerProps {}
+
+interface State {
+  key: string;
+}
+
+export default class SubScreenContainer extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      key: `${this.props.title},${this.props.flatList}`,
+    };
+  }
+  componentWillReceiveProps(nextProps, prevState) {
+    const key = `${nextProps.title}${this.props.flatList}`;
+    if (key !== prevState.key) {
+      this.setState({ key });
+    }
+  }
+  render() {
+    return (
+      <Collapsible
+        key={this.state.key}
+        max={45}
+        min={isIphoneX() ? 48 : 20}
+        backgroundColor={'white'}
+        renderHeader={
+          this.props.renderHeader
+            ? this.props.renderHeader()
+            : this.renderHeader()
+        }
+        renderContent={this.renderContent()}
+        contentContainerStyle={this.props.contentContainerStyle}
+        flatList={this.props.flatList}
+        data={this.props.data}
+        keyExtractor={this.props.keyExtractor}
+        renderItem={this.props.renderItem}
+      />
+    );
+  }
+  renderHeader() {
+    return (
+      <SubScreenHeader
+        title={this.props.title}
+        modal={this.props.modal}
+        icons={this.props.icons}
+      />
+    );
+  }
+  renderContent() {
+    return this.props.children;
+  }
+}

1e8e155ea88394d2c078a589daf8f04d789746f8
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-04-15 13:27:52 -0700

    Style recommendations screen

--- mobile/src/components/shared/Cards/RecommendationCard/RecommendationCard.tsx
+++ mobile/src/components/shared/Cards/RecommendationCard/RecommendationCard.tsx
@@ -2,13 +2,15 @@
  * RecommendationCard
  */
 
-import * as React from 'react';
-import { Text, View, TouchableOpacity, Image } from 'react-native';
+import * as React from "react";
+import { Text, View, TouchableOpacity, Image } from "react-native";
+import Swipeout from "react-native-swipeout";
 
-import styles from './RecommendationCard.styles';
-import { Recommendation } from '../../../../graphql/types';
-import { Icon } from 'react-native-elements';
-import NavigationService from '../../../../services/NavigationService';
+import styles from "./RecommendationCard.styles";
+import { Recommendation } from "../../../../graphql/types";
+import { Icon } from "react-native-elements";
+import NavigationService from "../../../../services/NavigationService";
+import ManageRollScreen from "../../../Search/ManageRollScreen";
 
 export interface Props {
   recommendation: Recommendation;
@@ -25,36 +27,78 @@ export default class RecommendationCard extends React.Component<Props, State> {
         recommendation.data.sources &&
         recommendation.data.sources[0]) ||
       {};
+    // let swipeBtns = [{
+    //   text: 'Delete',
+    //   backgroundColor: 'red',
+    //   underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+    //   onPress: () => { this.deleteNote(rowData) }
+    // }];
     return (
+      // <Swipeout right={swipeBtns}
+      //   autoClose='true'
+      //   backgroundColor= 'transparent'>
       <TouchableOpacity
         onPress={() => this.props.onPress(recommendation)}
         key={recommendation.id}
       >
         <View
-          style={{ width: '100%', alignItems: 'center' }}
+          style={{ width: "100%", alignItems: "center" }}
           key={recommendation.id}
         >
-          <View style={{ flexDirection: 'row', width: '100%' }}>
+          <View style={{ flexDirection: "row", width: "100%" }}>
             <Image style={styles.cover} source={{ uri: mainSource.cover }} />
-            <View style={{ flex: 1, justifyContent: 'center' }}>
+            <View style={{ flex: 1, justifyContent: "flex-start" }}>
               <Text style={styles.artist} numberOfLines={2}>
                 {mainSource.name}
               </Text>
-              <Text style={styles.noArtist} numberOfLines={2}>
-                {mainSource.creator}
+
+              {/* <Text style={styles.noArtist} numberOfLines={2}>
+                  {mainSource.creator}
+                </Text> */}
+
+              <Text style={styles.manageRoll}>
+                Recommended by: {recommendation.id}
               </Text>
+
+              {/* Manage Roll Button */}
+              <TouchableOpacity
+                onPress={() => {
+                  this.manageRoll(mainSource);
+                }}
+              >
+                <View>
+                  <Text style={styles.manageRoll}>Manage Roll</Text>
+                </View>
+              </TouchableOpacity>
+
+              {/* Delete Recommendation */}
+              <TouchableOpacity
+                onPress={() => {
+                  this.manageRoll(mainSource);
+                }}
+              >
+                <View>
+                  <Text style={styles.manageRoll}>Delete</Text>
+                </View>
+              </TouchableOpacity>
             </View>
             <Icon
               size={35}
-              name='more-vert'
-              color='lightgrey'
-              underlayColor='rgba(255,255,255,0)'
+              name="more-vert"
+              color="lightgrey"
+              underlayColor="rgba(255,255,255,0)"
               onPress={() => NavigationService.goBack()}
             />
           </View>
           <View style={styles.spacing} />
         </View>
       </TouchableOpacity>
+      // </Swipeout>
     );
+  }
+  manageRoll(source) {
+    NavigationService.navigate("ManageRoll", {
+      currentSource: source
+    });
   }
 }
